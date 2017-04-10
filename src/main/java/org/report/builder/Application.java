d15bb35ba53800ded98c20627804e6a587c588b8
Juanjo <jalejandro@europlayas.es>
2017-04-10 15:21:14 +0200

    Tomcat+

--- src/main/java/org/report/builder/Application.java
+++ src/main/java/org/report/builder/Application.java
@@ -2,16 +2,24 @@ package org.report.builder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
 @ComponentScan({"org.report.profile","org.report.controller", "org.report.services"})
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class,args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		logger.info("Se configura la aplicación");
+		return application.sources(Application.class);
+	}	
 	
 }
